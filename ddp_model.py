76279200fe606abfe78a6cee58eeb4d34449c741
denghilbert <971431670@qq.com>
2023-05-16 11:32:41 +0000

    nothing

--- ddp_model.py
+++ ddp_model.py
@@ -506,7 +506,7 @@ class NerfNetWithAutoExpo(nn.Module):
         elif img_name in self.env_params:
             env = self.env_params[img_name]
         else:
-            # logger.warning('no envmap found for ' + str(img_name))
+            logger.warning('no envmap found for ' + str(img_name))
             env = self.defaultenv
             # env = torch.tensor([
             #     [ 0.7953949,  0.4405923,  0.5459412],
--- ddp_test_nerf.py
+++ ddp_test_nerf.py
@@ -17,6 +17,20 @@ import logging
 
 
 logger = logging.getLogger(__package__)
+import pdb, sys
+class ForkedPdb(pdb.Pdb):
+    """A Pdb subclass that may be used
+    from a forked multiprocessing child
+
+    """
+
+    def interaction(self, *args, **kwargs):
+        _stdin = sys.stdin
+        try:
+            sys.stdin = open('/dev/stdin')
+            pdb.Pdb.interaction(self, *args, **kwargs)
+        finally:
+            sys.stdin = _stdin
 
 
 def ddp_test_nerf(rank, args):
